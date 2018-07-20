f343cec6a8ff608b710da0de9053c9006ac5289c
Guest User <Guest@epicodus-5.local>
2018-07-20 10:12:16 -0700

    Add views and controllers for many to many relationships between stylists and specialties

--- HairSalon/Controllers/SpecialtyController.cs
+++ HairSalon/Controllers/SpecialtyController.cs
@@ -24,5 +24,25 @@ namespace HairSalon.Controllers
       newSpecialty.Save();
       return RedirectToAction("CreateForm", Specialty.GetAll());
     }
+    [HttpGet("/specialty/stylist/{id}")]
+    public ActionResult Details(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist stylists = Stylist.Find(id);
+      List<Specialty> specialties = stylists.GetSpecialties();
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      model.Add("stylists", stylists);
+      model.Add("specialties", specialties);
+      model.Add("allSpecialties", allSpecialties);
+      return View(model);
+    }
+    [HttpPost("/books/{bookId}/books/new")]
+    public ActionResult AddAuthor(int bookId)
+    {
+      Stylist book = Stylist.Find(bookId);
+      Specialty author = Specialty.Find(int.Parse(Request.Form["author-id"]));
+      book.AddSpecialty(author);
+      return RedirectToAction("Details", new { id = bookId});
+    }
   }
 }
