c05e6f1296bea102f7a13fdb4db7e59105cd2375
Derek Hammer <derekhammer@hotmail.com>
2018-08-30 10:18:06 -0700

    Add tests for clients and specialties

--- HairSalon.Tests/ModelsTests/HairSalonTest.cs
+++ HairSalon.Tests/ModelsTests/HairSalonTest.cs
@@ -11,6 +11,8 @@ namespace HairSalon.Tests
     public void Dispose()
     {
       Stylist.DeleteAll();
+      Client.DeleteAll();
+      Specialty.DeleteAll();
     }
     public HairSalonTests()
         {
@@ -68,6 +70,77 @@ namespace HairSalon.Tests
       // Act
       Stylist.Find(testStylist.GetId()).Delete();
       List<Stylist> resultList = Stylist.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(testList, resultList);
+    }
+    [TestMethod]
+    public void Stylist_GetAll_Specialties()
+    {
+      List<Stylist> testList = new List<Stylist> {};
+      List<Stylist> resultList = Stylist.GetAll();
+
+      CollectionAssert.AreEqual(testList, resultList);
+    }
+    //Client tests
+    //
+    //
+    [TestMethod]
+    public void ClientNameIsTrue()
+    {
+
+      string name = "Ashley";
+      Client test = new Client(name, 1);
+      string result = test.GetClient();
+
+      Assert.AreEqual(name, result);
+    }
+    //Specialty Tests
+    //
+    //
+    [TestMethod]
+    public void SpecialtyTypeIsTrue()
+    {
+
+      string specialty = "curls";
+      Specialty test = new Specialty(specialty);
+      string result = test.GetSpecialty();
+
+      Assert.AreEqual(specialty, result);
+    }
+    [TestMethod]
+    public void Specialty_GetAll_Specialties()
+    {
+      List<Specialty> testList = new List<Specialty> {};
+      List<Specialty> resultList = Specialty.GetAll();
+
+      CollectionAssert.AreEqual(testList, resultList);
+    }
+    [TestMethod]
+    public void Save_SavesToDatabase_SpecialtyList()
+    {
+      //Arrange
+      Specialty testSpecialty = new Specialty("ombre");
+
+      //Act
+      testSpecialty.Save();
+      List<Specialty> result = Specialty.GetAll();
+      List<Specialty> testList = new List<Specialty>{testSpecialty};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+    [TestMethod]
+    public void Delete_DeleteSpecialty()
+    {
+      // Arrange
+      List<Specialty> testList = new List<Specialty> {};
+      Specialty testSpecialty = new Specialty("mohawk");
+      testSpecialty.Save();
+
+      // Act
+      Specialty.Find(testSpecialty.GetId()).Delete();
+      List<Specialty> resultList = Specialty.GetAll();
 
       // Assert
       CollectionAssert.AreEqual(testList, resultList);
